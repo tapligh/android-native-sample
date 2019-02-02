c507267d56db0eff07d29935c927329ef0926454
tapligh <developers@tapligh.com>
2019-02-02 15:09:21 +0330

    add fabric tracker in order to register and reporting crash

--- app/src/main/java/com/tapligh/android/sample_native/App.java
+++ app/src/main/java/com/tapligh/android/sample_native/App.java
@@ -3,6 +3,8 @@ package com.tapligh.android.sample_native;
 import android.app.Application;
 
 import com.tapligh.sdk.nativead.Tapligh;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * CREATED BY Javadroid FOR `android-native-sample` PROJECT
@@ -14,6 +16,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         new Tapligh.Builder(this, "9df6a91a-fd3e-482f-8e04-8bb58d8573c4").build();
 
