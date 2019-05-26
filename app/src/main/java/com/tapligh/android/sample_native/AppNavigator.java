d3ee4c451c1ac00b5d66aaf7d3023813860fc6dd
tapligh <developers@tapligh.com>
2019-05-26 17:34:04 +0430

    remove current classes

--- app/src/main/java/com/tapligh/android/sample_native/AppNavigator.java
+++ app/src/main/java/com/tapligh/android/sample_native/AppNavigator.java
@@ -1,14 +1,10 @@
 package com.tapligh.android.sample_native;
 
 import android.content.Context;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 
-import com.tapligh.android.sample_native.chooser.ChooserPageFragment;
-import com.tapligh.android.sample_native.grid.EndlessGridFragment;
-import com.tapligh.android.sample_native.list.EndlessListFragment;
-import com.tapligh.android.sample_native.simple.SimpleNativeAdFragment;
+import com.tapligh.android.sample_native.view.ChooserPageFragment;
 
 /**
  * CREATED BY Javadroid FOR `android-native-sample` PROJECT
@@ -38,34 +34,4 @@ public class AppNavigator {
         changeFragment(fragment, ChooserPageFragment.class.getName());
     }
 
-    public void gotoSimpleAdFragment(Context context) {
-        SimpleNativeAdFragment fragment = (SimpleNativeAdFragment) SimpleNativeAdFragment
-                .instantiate(context, SimpleNativeAdFragment.class.getName());
-        changeFragment(fragment, SimpleNativeAdFragment.class.getName());
-    }
-
-    public void gotoEndlessListFragment(Context context) {
-        EndlessListFragment fragment = (EndlessListFragment) EndlessListFragment
-                .instantiate(context, EndlessListFragment.class.getName());
-        changeFragment(fragment, EndlessListFragment.class.getName());
-    }
-
-    public void gotoSimpleEndlessGridFragment(Context context) {
-        Bundle bundle = new Bundle();
-        bundle.putInt("step" , 1);
-
-        EndlessGridFragment fragment = (EndlessGridFragment) EndlessGridFragment
-                .instantiate(context, EndlessGridFragment.class.getName(), bundle);
-        changeFragment(fragment, EndlessGridFragment.class.getName());
-    }
-
-
-    public void gotoBannerEndlessGridFragment(Context context){
-        Bundle bundle = new Bundle();
-        bundle.putInt("step" , 2);
-
-        EndlessGridFragment fragment = (EndlessGridFragment) EndlessGridFragment
-                .instantiate(context, EndlessGridFragment.class.getName(), bundle);
-        changeFragment(fragment, EndlessGridFragment.class.getName());
-    }
 }
